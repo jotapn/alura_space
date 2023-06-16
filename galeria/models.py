d0f5ef1d774ff8c63a07e9182d51032f10bdc5cb
Pádua Neto <paduacn@gmail.com>
2023-06-15 21:43:02 -0300

    Aula 02 - ADMIN

--- galeria/models.py
+++ galeria/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class Fotografia(models.Model):
+    OPCAO_CATEGORIA=[
+        ('NEBULOSA', 'Nebulosa'),
+        ('GALÁXIA', 'Galáxia'),
+        ('ESTRELA', 'Estrela'),
+        ('PALNETA', 'Planeta')
+    ]
+
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=100, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default='')
     descricao= models.TextField(max_length=100, null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
