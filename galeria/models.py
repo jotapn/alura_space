07c7da0fc5d7ed735e51a82a476a1f5460b986d4
Pádua Neto <paduacn@gmail.com>
2023-06-20 15:31:26 -0300

    Aula 04 - Imagens e filtros. Adicionado filtros na página do admin e campo de upload de imagens

--- galeria/models.py
+++ galeria/models.py
@@ -7,16 +7,16 @@ class Fotografia(models.Model):
         ('NEBULOSA', 'Nebulosa'),
         ('GALÁXIA', 'Galáxia'),
         ('ESTRELA', 'Estrela'),
-        ('PALNETA', 'Planeta')
+        ('PLANETA', 'Planeta')
     ]
 
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=100, null=False, blank=False)
     categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default='')
     descricao= models.TextField(max_length=100, null=False, blank=False)
-    foto = models.FileField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
     publicada = models.BooleanField(default=False)
     data_publicada = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self) -> str:
-        return f'Fotografia [nome={self.nome}]'
+        return self.nome
