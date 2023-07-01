30d359766ea9b4936018228545101121cf78a131
Pádua Neto <paduacn@gmail.com>
2023-07-01 00:27:36 -0300

    Refatorando o código e corrigindo bugs

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.tests import FotografiaForms
+
+
 
 from django.contrib import messages
 
@@ -25,3 +28,21 @@ def buscar(request):
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
     
     return render(request, 'galeria/buscar.html',{'cards': fotografias})
+
+def nova_imagem(request):
+    form = FotografiaForms
+    return render(request, 'galeria/nova_imagem.html', {'form', form})
+
+def editar_imagem(request):
+    pass
+
+def deletar_imagem(request):
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    if 'deletar' in request.GET:
+        fotografias = Fotografia.objects.delete
+        return redirect('index')
+
+
+    return redirect('index')
